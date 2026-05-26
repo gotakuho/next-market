0e900db553c8ee2370dc3eb5aec486347534eb06
gotakuho <251008@st.yoshida-g.ac.jp>
2026-05-26 12:05:04 +0900

    ４

--- app/page.js
+++ app/page.js
@@ -1,31 +1,50 @@
 import Link from "next/link"
 import Image from "next/image"
 
-const getAllItems = async() => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/readall', {cache: "no-store"})
+const getAllItems = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/readall`, {
+        cache: "no-store"
+    })
+
     const jsonData = await response.json()
     const allItems = jsonData.allItems
+
     return allItems
 }
 
-const ReadAllItems = async() => {
+const ReadAllItems = async () => {
     console.log(process.env.NEXT_PUBLIC_URL)
+
     const allItems = await getAllItems()
+
     return (
         <div className="grid-container-in">
-            {allItems.map(item => 
+            {allItems.map(item =>
                 <Link href={`/item/readsingle/${item._id}`} key={item._id}>
-                   {item.image ? (<Image src={item.image} width={750} height={500} alt="item-image" priority />
-                       ) : null}
-                    <div> 
+                    {item.image ? (
+                        <Image
+                            src={item.image}
+                            width={750}
+                            height={500}
+                            alt="item-image"
+                            priority
+                        />
+                    ) : null}
+
+                    <div>
                         <h2>¥{item.price}</h2>
                         <h3>{item.title}</h3>
-                        <p>{item.description ? item.description.substring(0, 80) : ""}...</p>
+                        <p>
+                            {item.description
+                                ? item.description.substring(0, 80)
+                                : ""}
+                            ...
+                        </p>
                     </div>
                 </Link>
             )}
         </div>
     )
-} 
+}
 
 export default ReadAllItems
